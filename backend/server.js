300459a715bab80a4242eb49a0a0bd64d0c822a5
JustHarshit <hrpal00154s@gmail.com>
2025-07-14 11:22:54 +0530

    Added Morgan Logging

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,21 @@ import authRoutes from './routes/auth.js';
 import auctionRoutes from './routes/auctions.js';
 import helmet from 'helmet';
 import errorHandler from './middleware/errorHandler.js';
+import fs from 'fs';
+import path from 'path';
 import morgan from 'morgan';
+
 
 dotenv.config(); 
 
 const app = express();
 const port = process.env.PORT || 5001;
+// Create a write stream (in append mode)
+const accessLogStream = fs.createWriteStream(
+  path.join(process.cwd(), 'access.log'),
+  { flags: 'a' }
+);
+
 
 // Middleware and CORS setup
 app.use(helmet());
@@ -27,6 +36,9 @@ app.use('/auth', authRoutes);
 app.use('/auctions', auctionRoutes);
 app.use(errorHandler); // Error handling middleware
 app.use(morgan('dev')); // Logging middleware
+// Setup the logger to write to the file
+app.use(morgan('combined', { stream: accessLogStream }));
+
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGODB_URI)
